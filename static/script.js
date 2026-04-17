d1eff44b2f78b931572b17943ffbad6cbe079a34
potecchi-E <potecchi.edoardo@iisgalvanimi.edu.it>
2026-04-17 07:46:23 +0000

    da fare server

---
+++ static/script.js
@@ -0,0 +1,19 @@
+async function controllaCredenziali() {
+    // 1. Prendiamo quello che l'utente ha scritto
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (username || password) {
+        return alert("Scrivi un username e password")
+    }
+
+    // 2. Chiamiamo il server passando lo username e password nell'URL (?username=...)
+    const res = await fetch(`/login?username=${username}&password=&{password}`);
+    const dati = await res.json();
+
+    // 3. Mostriamo la risposta del server nella pagina
+    document.getElementById('risultato').innerText = dati.messaggio;
+}
+
+// Colleghiamo i bottoni alle funzioni
+document.getElementById('btn_registrati').addEventListener('click', controllaCredenziali);
