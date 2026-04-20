4059d36ce1b585b4252669c807582f94d8af5b53
potecchi-E <potecchi.edoardo@iisgalvanimi.edu.it>
2026-04-20 09:21:57 +0000

    Completato con get

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-# Spieghiamo a FastAPI che i file dentro "static" sono accessibili
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/") # End point è il punto dove chiamiamo il nostro server web, lo / è la homepage
+@app.get("/")
 def home():
-    # Restituisce direttamente il file HTML
     return FileResponse('static/index.html')
 
+
 @app.get("/login")
-def controlla(username:str, password:str):
-    print('Username', username, 'password', password)
+def controlla(username: str, password: str):
+
     if username == 'admin' and password == 'xxx123':
-        risposta = {'messaggio' : 1}
+        return {"messaggio": 1}
+            
     else: 
-        risposta = {'messaggio' : 0}
-    return risposta
+        return { "messaggio": 0}
+       
--- static/script.js
+++ static/script.js
@@ -1,5 +1,5 @@
 async function controllaCredenziali() {
-    // 1. Prendiamo quello che l'utente ha scritto
+    // 1. Prendiamo gli input dell'utente
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
@@ -7,13 +7,31 @@ async function controllaCredenziali() {
         return alert("Scrivi un username e una password")
     }
 
-    // 2. Chiamiamo il server passando lo username e password nell'URL (?username=...)
+    // 2. Chiamiamo il server
     const res = await fetch(`/login?username=${username}&password=${password}`);
     const dati = await res.json();
 
-    // 3. Mostriamo la risposta del server nella pagina
-    document.getElementById('risultato').innerText = dati.messaggio;
+    // 3. Controlliamo il messaggio del server
+    if (dati.messaggio === 1) {
+        document.getElementById('risultato').innerText = "Accesso efettuato con successo"; // inner text serve a leggere il contenuto dell'elemento in html (es. <p>)
+
+        // rende gli input non modificabili
+        document.getElementById('username').disabled = true;
+        document.getElementById('password').disabled = true;
+
+        // rimuove il bottone
+        document.getElementById('btn_registrati').remove();
+    } 
+    
+    else if (dati.messaggio === 0) { // "===" confronta oltre al valore il tipo della variabile
+        document.getElementById('risultato').innerText = "Username o password errati";
+
+        // svuota input
+        document.getElementById('username').value = "";
+        document.getElementById('password').value = "";
+        document.getElementById('username').focus(); // .focus() rimette il cursore su un elemento, è vuoto quindi lo mette sul primo disponibile
+    }
 }
 
-// Colleghiamo i bottoni alle funzioni
+// collegamento click del bottone alla funzione 
 document.getElementById('btn_registrati').addEventListener('click', controllaCredenziali);
