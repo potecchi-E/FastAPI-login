3d7472e5434807f10d2a70fb7dcc1b6cc6ad0846
potecchi-E <potecchi.edoardo@iisgalvanimi.edu.it>
2026-04-23 10:55:35 +0000

    risolvere errori

--- static/script.js
+++ static/script.js
@@ -1,37 +1,27 @@
-async function controllaCredenziali() {
-    // 1. Prendiamo gli input dell'utente
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+async function loggati() {
 
-    if (!username || !password) {
-        return alert("Scrivi un username e una password")
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (!username || !password)
+        return alert("Scrivi lo username e password");
+
+    const res = await fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: `username=${username}&password=${password}`
+    });
+
+    const json = await res.json();
+
+    if (json.messaggio == 1){
+        document.getElementById("risultato").innerText = "Accesso effettuato";
     }
-
-    // 2. Chiamiamo il server
-    const res = await fetch(`/login?username=${username}&password=${password}`);
-    const dati = await res.json();
-
-    // 3. Controlliamo il messaggio del server
-    if (dati.messaggio === 1) {
-        document.getElementById('risultato').innerText = "Accesso efettuato con successo"; // inner text serve a leggere il contenuto dell'elemento in html (es. <p>)
-
-        // rende gli input non modificabili
-        document.getElementById('username').disabled = true;
-        document.getElementById('password').disabled = true;
-
-        // rimuove il bottone
-        document.getElementById('btn_registrati').remove();
-    } 
-    
-    else if (dati.messaggio === 0) { // "===" confronta oltre al valore il tipo della variabile
-        document.getElementById('risultato').innerText = "Username o password errati";
-
-        // svuota input
-        document.getElementById('username').value = "";
-        document.getElementById('password').value = "";
-        document.getElementById('username').focus(); // .focus() rimette il cursore su un elemento, è vuoto quindi lo mette sul primo disponibile
+    else {
+        document.getElementById("risultato").innerText = "Accesso negato";
     }
 }
 
-// collegamento click del bottone alla funzione 
-document.getElementById('btn_registrati').addEventListener('click', controllaCredenziali);
+document.getElementById('bottone').addEventListener('click', loggati);
